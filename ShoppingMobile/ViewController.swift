8706228f3fe8ae467d8152d9fdfb79bed8e4777d
stray s <strays@straydeMacBook-Pro.local>
2017-08-01 21:49:20 +0800

    Initial Commit

---
+++ ShoppingMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoppingMobile
+//
+//  Created by stray s on 2017/8/1.
+//  Copyright © 2017年 stray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
